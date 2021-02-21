e167df400675c1dce6779a53c64584727a7a5a33
Alex Andrew <pytormal@live.com>
2021-02-21 15:05:57 -0800

    added Private Route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,22 @@
-
-
-
-
-
-
-
-
-
 //Task List:
 //1. Build a PrivateRoute component that redirects if user is not logged in
+
+
+
+
+
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom'
+
+const PrivateRoute = ({ component: Component }) => {
+    return <Route {...Component} render={(props) => {
+        if (localStorage.getItem('token')) {
+            return <Component {...props} />
+        }
+        else {
+            return <Redirect {...alert('you need to login first')} to='/' />
+        }
+    }}  />
+}
+
+export default PrivateRoute;
